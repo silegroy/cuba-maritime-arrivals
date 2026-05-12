d3a2204482595e1880b98d63e1399480bd4f23fa
silegroy <43544658+silegroy@users.noreply.github.com>
2026-05-12 19:53:17 +0000

    mapa de cuba

--- app.py
+++ app.py
@@ -22,3 +22,27 @@ if st.button("🚢 Ejecutar scraper real"):
     run_real_scraper()
     st.success("Scraper ejecutado correctamente")
     st.cache_data.clear()
+
+st.subheader("📍 Puertos de Cuba")
+
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    data=CUBA_PORTS,
+    get_position="[lon, lat]",
+    get_radius=7000,
+    get_color=[200, 30, 0],
+)
+
+view_state = pdk.ViewState(
+    latitude=22,
+    longitude=-80,
+    zoom=5,
+)
+
+st.pydeck_chart(
+    pdk.Deck(
+        layers=[layer],
+        initial_view_state=view_state
+    )
+)
+
